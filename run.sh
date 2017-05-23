eb54502be814e2f209c29e04d2d57e7e80850313
martin <notinventedhere8@gmail.com>
2017-05-23 13:18:00 +0100

    run.sh file added and idea files.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if pgrep Live; then
+    osascript -e 'tell application "System Events"
+        set frontmost of process "Live" to true
+        keystroke "q" using command down
+        tell application "System Events" to keystroke (ASCII character 28) --left arrow
+        tell application "System Events" to keystroke (ASCII character 28) --left arrow
+        keystroke return
+    end tell'
+fi
+
+while pgrep Live; do
+  sleep 1
+done
+
+find . -name \*.pyc -delete
+
+open -a 'Ableton Live 9 Lite.app'
